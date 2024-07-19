e05194ea90b8d4c145e6d2cbcc90e89fd5ca46d8
nasreddinesoltani <soltani.nasro@gmail.com>
2024-07-19 20:41:16 +0100

    algo3

---
+++ script.js
@@ -0,0 +1,39 @@
+// function LeapYear (year){
+//     return year%4 == 0 && year%100 !== 0 || year % 400 === 0
+// }
+
+// console.log(LeapYear(2020));
+// console.log(LeapYear(2021));
+// console.log(LeapYear(1700));
+// console.log(LeapYear(220));
+// console.log(LeapYear(2024));
+
+// function AgetoPrice ( Age){
+//     return Age <= 12 ? '10$' :Age >13 && Age <17 ?'15$' : '20$'
+// }
+
+// console.log(AgetoPrice(20));
+// console.log(AgetoPrice(2));
+// console.log(AgetoPrice(15));
+
+
+// function recursing  (n){
+//     if ( n < 2){
+//         return n
+//     }
+//     return recursing(n-1)+ recursing(n-2)
+    
+// }
+
+// console.log(recursing(3));
+
+
+// function palydrome (str){
+//     return str.split('').reverse().join('') === str  ;
+
+// }
+
+// console.log(palydrome('AZA'));
+
+
+
